2fcd64adf54005c909859809a174affce6498ae7
Mauricio Serna Flórez <mauricioserf@hotmail.com>
2015-11-12 20:14:13 -0500

    Delete unnecesary files, add default image for shirt model (necesary in paperclip)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
 	def index
-		
+		# @shirt = Shirt.all
 	end
 end
--- app/models/shirt.rb
+++ app/models/shirt.rb
@@ -1,6 +1,6 @@
 class Shirt < ActiveRecord::Base
-	 has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	 has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, :default_url => ActionController::Base.helpers.asset_path('missing.png')
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
   
-	validates_presence_of(:size, :quantity, :photo, :description, :price)
+	validates_presence_of(:size, :quantity, :description, :price)
 end
