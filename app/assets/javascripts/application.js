3ad364bb0538362bf10183b95bd888706bae443c
fmauricios <mauricioserf@hotmail.com>
2015-11-19 12:24:35 -0500

    Add images for know more

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,5 +16,18 @@
 //= require turbolinks
 //= require bootstrap
 //= require bootstrap.min
-//= require_tree .
+//= require_tree 
+
+$(document).ready(function() {
+  $("[rel='tooltip']").tooltip();
+
+  $('.thumbnail').hover(
+    function(){
+      $(this).find('.caption').slideDown(250); //.fadeIn(250)
+    },
+    function(){
+      $(this).find('.caption').slideUp(250); //.fadeOut(205)
+    }
+  ); 
+});
 
