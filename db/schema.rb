7f3a0a665b98bd2020d3db04ce9da99ea2f2db6d
fmauricios <mauricioserf@hotmail.com>
2015-11-19 22:01:54 -0500

    Modify schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151113145743) do
+ActiveRecord::Schema.define(version: 20151119173902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20151113145743) do
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
+    t.string   "title"
   end
 
   create_table "shirts_categories", id: false, force: :cascade do |t|
